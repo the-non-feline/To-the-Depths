8d1bcc723424f4033046251ba9d79b5fc5f7ec56
the-non-feline <32406395+the-non-feline@users.noreply.github.com>
2018-12-17 21:15:53 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,4 @@
 import setuptools
 
-requires = [] 
-
-with open('requirements.txt') as file: 
-  requires = file.read().splitlines() 
-
-setuptools.setup(name='To the Depths', version='0.1', packages=['to_the_depths'], install_requires=requires) 
+setuptools.setup(name='To the Depths', version='0.1', packages=['to_the_depths'], 
+                 dependency_links=['https://github.com/Rapptz/discord.py/rewrite']) 
