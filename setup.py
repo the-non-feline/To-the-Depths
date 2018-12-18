86f8e6de1e606f290670305145f8ec1ed4ba37c5
the-non-feline <32406395+the-non-feline@users.noreply.github.com>
2018-12-17 20:52:56 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,8 @@
 import setuptools
 
+requires = [] 
+
+with open('requirements.txt') as file: 
+  requires = file.read().splitlines() 
+
 setuptools.setup(name='To the Depths', version='0.1', packages=['to_the_depths']) 
