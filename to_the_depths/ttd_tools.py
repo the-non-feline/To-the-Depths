7f66b0bc717014b46c11afc88ed1814804a2cd64
the-non-feline <32406395+the-non-feline@users.noreply.github.com>
2018-12-17 18:10:07 -0800

    Add files via upload

---
+++ to_the_depths/ttd_tools.py
@@ -0,0 +1,34 @@
+import builtins
+import time
+import discord
+
+def print(*values, sep=' ', end='\n'): 
+  builtins.print('{}: '.format(time.asctime()), end='') 
+  builtins.print(*values, sep=sep, end=end + '\n') 
+
+import storage
+
+class Game_Object(storage.Deconstructable): 
+  def __init__(self, client, channel): 
+    self.client = client
+    self.channel = channel
+
+    storage.Deconstructable.__init__(self) 
+  
+  def modify_deconstructed(self, deconstructed): 
+    del deconstructed['client'], deconstructed['channel'] 
+
+    storage.Deconstructable.modify_deconstructed(self, deconstructed) 
+  
+  def on_shutdown(self): 
+    pass
+  
+  async def on_turn_on(self): 
+    pass
+  
+  @classmethod
+  def class_embed(self): 
+    return discord.Embed(type='rich')  
+  
+  def object_embed(self): 
+    return self.class_embed() 
