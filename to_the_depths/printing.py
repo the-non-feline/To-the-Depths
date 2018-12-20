4a2a1e3153e67c5cb517cd3f2092d90f357b8716
the-non-feline <32406395+the-non-feline@users.noreply.github.com>
2018-12-19 20:03:24 -0800

    Update printing.py

--- to_the_depths/printing.py
+++ to_the_depths/printing.py
@@ -4,3 +4,4 @@ import time
 def print(*values, sep=' ', end='\n'): 
 	builtins.print('{} - '.format(time.asctime()), end='', flush=True) 
 	builtins.print(*values, sep=sep, end=end + '\n', flush=True) 
+ 
