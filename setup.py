00dd5c5a37eb0c18e848ea2ba2edfdf980b28fd0
the-non-feline <32406395+the-non-feline@users.noreply.github.com>
2018-12-21 10:36:47 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import setuptools
 
 setuptools.setup(name='To the Depths', version='0.1', packages=['to_the_depths'], 
-								 install_requires=['discord.py @ git+https://github.com/Rapptz/discord.py.git@rewrite'], 
-								 setup_requires=['discord.py @ git+https://github.com/Rapptz/discord.py.git@rewrite']) 
+								 install_requires=['discord.py @ dkjfhdjshfkdshfjksdhfjkdshfkjsdhf'], 
+								 setup_requires=['discord.py @ dkfjhkdsjflksdjflkdsjflksdfj']) 
