366e7bcc396ffd2c022d6a1320f1a328edce8342
the-non-feline <32406395+the-non-feline@users.noreply.github.com>
2018-12-21 10:30:00 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
 import setuptools
 
-setuptools.setup(name='To the Depths', version='0.1', packages=['to_the_depths'], install_requires=['discord@https://github.com/Rapptz/discord.py/tarball/rewrite#egg=discord'], setup_requires=['discord@https://github.com/Rapptz/discord.py/tarball/rewrite#egg=discord'])  
+setuptools.setup(name='To the Depths', version='0.1', packages=['to_the_depths'], 
+								 install_requires=['discord @ https://github.com/Rapptz/discord.py.git@rewrite'], 
+								 setup_requires=['discord @ https://github.com/Rapptz/discord.py.git@rewrite']) 
