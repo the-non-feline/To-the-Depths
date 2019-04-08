2ab4ed0e2d6da16e1a4eb2786b1b6d5c9897b1f9
the-non-feline <32406395+the-non-feline@users.noreply.github.com>
2019-04-08 16:49:49 -0700

    Delete printing.py

--- to_the_depths/printing.py
+++
@@ -1,7 +0,0 @@
-import builtins
-import time
-
-def print(*values, sep=' ', end='\n'): 
-	builtins.print('{} - '.format(time.asctime()), end='', flush=True) 
-	builtins.print(*values, sep=sep, end=end + '\n', flush=True) 
- 
