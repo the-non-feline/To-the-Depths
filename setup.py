06c49807746a1367098fc22b832fccdf7b804caa
the-non-feline <32406395+the-non-feline@users.noreply.github.com>
2019-11-10 18:57:14 -0800

    increased version

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 import setuptools
 
-setuptools.setup(name='To the Depths Beta', version='0.48.5', packages=('to_the_depths_beta',), install_requires=('discord.py',), setup_requires=('discord.py',)) 
+setuptools.setup(name='To the Depths Beta', version='0.48.6', packages=('to_the_depths_beta',), install_requires=('discord.py',), setup_requires=('discord.py',)) 
